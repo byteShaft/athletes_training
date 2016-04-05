fcc1504ef12c96b19d6e20cac88f91366b1f2c4a
s9iper1 <s9iper1@gmail.com>
2016-04-05 21:26:46 +0500

    work

--- app/src/main/java/com/pits/athletestraining/activities/UserInfoActivity.java
+++ app/src/main/java/com/pits/athletestraining/activities/UserInfoActivity.java
@@ -2,6 +2,7 @@ package com.pits.athletestraining.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import com.pits.athletestraining.R;
 
@@ -12,5 +13,11 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
